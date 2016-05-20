da5cdb4d89fcc6ae5fa469cca68d4a57c3d4ba07
detective7 <detective.ive@163.com>
2016-05-20 17:35:34 +0800

    总结，鸿洋大神加多了padding等的百分比例显示

--- myPercentSupportDemo/src/main/java/com/example/ys/mypercentsupportdemo/Main2Activity.java
+++ myPercentSupportDemo/src/main/java/com/example/ys/mypercentsupportdemo/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.ys.mypercentsupportdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView tv1 = (TextView) this.findViewById(R.id.tv1);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this,Main3Activity.class);
+                Main2Activity.this.startActivity(intent);
+                Main2Activity.this.finish();
+            }
+        });
     }
 }
